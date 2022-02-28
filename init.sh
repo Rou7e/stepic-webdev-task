f0c60074c0ddba2595865d00b8d6577519cb9ed4
Rou7e <90070435+Rou7e@users.noreply.github.com>
2022-03-01 00:56:32 +0300

    attempt 2

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-gunicorn -c /home/box/web/etc/ask.py ask.wsgi &
-gunicorn -c /home/box/web/etc/hello.py hello:app
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
